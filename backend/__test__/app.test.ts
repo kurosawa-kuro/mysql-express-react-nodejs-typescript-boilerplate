9f20501be688f7192f40b74596443923966b3b52
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-07-04 00:01:39 +0900

    fix GET /endpoints

--- backend/__test__/app.test.ts
+++ backend/__test__/app.test.ts
@@ -19,5 +19,16 @@ describe("GET /endpoints", () => {
 
     expect(response.status).toBe(200);
     expect(Array.isArray(response.body)).toBe(true);
+
+    const endpoints = response.body;
+    for (const endpoint of endpoints) {
+      expect(endpoint).toHaveProperty("path");
+      expect(endpoint).toHaveProperty("methods");
+      expect(endpoint).toHaveProperty("middlewares");
+
+      expect(typeof endpoint.path).toBe("string");
+      expect(Array.isArray(endpoint.methods)).toBe(true);
+      expect(Array.isArray(endpoint.middlewares)).toBe(true);
+    }
   });
 });
