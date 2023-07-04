041d8cd5aef592f15d63e541f7ec6ffd263624ab
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-07-04 16:14:13 +0900

    f

--- frontend/src/__test__/InformationScreen.test.tsx
+++ frontend/src/__test__/InformationScreen.test.tsx
@@ -1,26 +1,28 @@
 // frontend\src\__test__\InformationScreen.test.tsx
 
 import { render, screen, waitFor } from "@testing-library/react";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
-import { HomeScreen } from "../screens/HomeScreen";
-import { simulateLogin } from "./testUtils";
 import { App } from "../App";
 import { InformationScreen } from "../screens/InformationScreen";
 
-const server = setupServer(
-  rest.get("http://localhost:8080/api/", (_req, res, ctx) => {
-    return res(ctx.text("API is running...."));
-  })
-);
+describe("Rendering InformationScreen", () => {
+  it("displays the InformationScreen upon navigation", async () => {
+    render(
+      <MemoryRouter initialEntries={["/information"]}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/information" element={<InformationScreen />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+    await screen.findByRole("heading", { name: /Information/i });
+  });
+});
 
-describe("HomeScreen", () => {
-  it("renders the HomeScreen and displays the API status when API request is successful", async () => {
+describe("Retrieving Information Data", () => {
+  it("shows Information Data when API request is successful", async () => {
     render(
       <MemoryRouter initialEntries={["/information"]}>
         <Routes>
@@ -33,29 +35,6 @@ describe("HomeScreen", () => {
 
     await waitFor(() =>
       expect(screen.getByText("Information Data")).toBeInTheDocument()
-    );
-  });
-
-  it("displays an error message when the API request fails", async () => {
-    server.use(
-      rest.get("http://localhost:8080/api/", (_req, res, ctx) => {
-        return res(ctx.status(500), ctx.json({ message: "Server error" }));
-      })
-    );
-
-    render(
-      <MemoryRouter initialEntries={["/"]}>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="/" element={<HomeScreen />} />
-          </Route>
-        </Routes>
-      </MemoryRouter>
-    );
-
-    await simulateLogin();
-    await waitFor(() =>
-      expect(screen.getByText("Server error")).toBeInTheDocument()
     );
   });
 });
