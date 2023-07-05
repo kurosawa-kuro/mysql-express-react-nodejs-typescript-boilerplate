9c6aae3bec4069f3e292f1557b9a9dfea281e0ef
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-07-06 08:35:01 +0900

    fix endpoints

--- backend/index.ts
+++ backend/index.ts
@@ -54,8 +54,14 @@ if (NODE_ENV === "production") {
   app.get("/api/", (_req, res) => {
     res.send("API is running....");
   });
-  app.get("/api/endpoints", (req, res) => {
-    res.send(expressListEndpoints(app));
+  app.get("/api/endpoints", (request, response) => {
+    const originalEndpoints = expressListEndpoints(app);
+
+    const sanitizedEndpoints = originalEndpoints.map((endpoint) => {
+      const { middlewares, ...sanitizedEndpoint } = endpoint;
+      return sanitizedEndpoint;
+    });
+    response.send(sanitizedEndpoints);
   });
 }
 
