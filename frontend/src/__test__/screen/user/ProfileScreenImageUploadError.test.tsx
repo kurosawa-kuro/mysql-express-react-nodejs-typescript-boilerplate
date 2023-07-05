41f820c157294e763dceaebf5dc3bc072031cf02
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-07-05 20:29:18 +0900

    fix ProfileScreenImageUploadError.test

--- frontend/src/__test__/screen/user/ProfileScreenImageUploadError.test.tsx
+++ frontend/src/__test__/screen/user/ProfileScreenImageUploadError.test.tsx
@@ -1,13 +1,13 @@
 // frontend\src\screens\product\HomeScreen.test.tsx
 
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
-import userEvent from "@testing-library/user-event";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
 import { simulateLogin } from "../../testUtils";
 import { App } from "../../../App";
 import { ProfileScreen } from "../../../screens/user/ProfileScreen";
+import userEvent from "@testing-library/user-event";
 
 const uploadImageResponse = {
   image: "url-to-your-image",
@@ -26,6 +26,11 @@ jest.mock("../../../services/api", () => ({
   uploadImage: jest.fn(() => Promise.resolve(uploadImageResponse)),
 }));
 
+jest.mock("../../../services/api", () => ({
+  ...jest.requireActual("../../../services/api"),
+  uploadImage: jest.fn(() => Promise.reject(new Error("Test error message"))),
+}));
+
 const server = setupServer(
   rest.put("http://localhost:8080/api/users/profile", (_req, res, ctx) => {
     return res(ctx.json(userProfileResponse));
@@ -36,7 +41,7 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-describe("ProfileScreen", () => {
+describe("ProfileScreen ", () => {
   it("renders the ProfileScreen and displays the API status when API request is successful", async () => {
     render(
       <MemoryRouter initialEntries={["/profile"]}>
@@ -58,18 +63,6 @@ describe("ProfileScreen", () => {
 
     expect(nameInput).toBeInTheDocument();
     expect(emailInput).toBeInTheDocument();
-
-    const file = new File(["dummy content"], "test.png", { type: "image/png" });
-    const inputFile = screen.getByLabelText("Image File") as HTMLInputElement;
-    userEvent.upload(inputFile, file);
-
-    const [uploadMessage, imageInput] = await Promise.all([
-      screen.findByText(uploadImageResponse.message),
-      screen.findByDisplayValue(uploadImageResponse.image),
-    ]);
-
-    expect(uploadMessage).toBeInTheDocument();
-    expect(imageInput).toBeInTheDocument();
 
     // ======
     const passwordInputElement = screen.getByPlaceholderText(
@@ -102,5 +95,88 @@ describe("ProfileScreen", () => {
     ]);
 
     expect(updateMessage).toBeInTheDocument();
+  });
+
+  it("updateUserProfile fail", async () => {
+    // jest.mock("../../../services/api", () => ({
+    //   ...jest.requireActual("../../../services/api"),
+    //   updateUserProfile: jest.fn(() =>
+    //     Promise.reject(new Error("Test error message"))
+    //   ),
+    // }));
+
+    // server.useでrest.put("http://localhost:8080/api/users/profile"が失敗する設定
+    server.use(
+      rest.put("http://localhost:8080/api/users/profile", (_req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({ message: "Server error" }));
+      })
+    );
+
+    // const server = setupServer(
+    //   rest.put("http://localhost:8080/api/users/profile", (_req, res, ctx) => {
+    //     return res(ctx.json(userProfileResponse));
+    //   })
+    // );
+    render(
+      <MemoryRouter initialEntries={["/profile"]}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/profile" element={<ProfileScreen />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    await simulateLogin();
+    await screen.findByRole("heading", { name: /User Profile/i });
+
+    const passwordInputElement = screen.getByPlaceholderText(
+      "Enter password"
+    ) as HTMLInputElement;
+    fireEvent.change(passwordInputElement, { target: { value: "password" } });
+
+    const confirmPasswordInputElement = screen.getByPlaceholderText(
+      "Confirm password"
+    ) as HTMLInputElement;
+    fireEvent.change(confirmPasswordInputElement, {
+      target: { value: "password" },
+    });
+
+    const updateButton = await screen.findByRole("button", { name: /Update/i });
+    fireEvent.click(updateButton);
+
+    // Verify that the error toast is displayed
+    await waitFor(() =>
+      expect(screen.getByText("Server error")).toBeInTheDocument()
+    );
+  });
+
+  it("renders the ProfileScreen and displays the API status when API request is successful", async () => {
+    render(
+      <MemoryRouter initialEntries={["/profile"]}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/profile" element={<ProfileScreen />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    await simulateLogin();
+    await screen.findByRole("heading", { name: /User Profile/i });
+
+    server.use(
+      rest.post("http://localhost:8080/api/upload", (_req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({ message: "Server error" }));
+      })
+    );
+
+    const file = new File(["dummy content"], "test.png", { type: "image/png" });
+    const inputFile = screen.getByLabelText("Image File") as HTMLInputElement;
+    userEvent.upload(inputFile, file);
+
+    await waitFor(() =>
+      expect(screen.getByText("Test error message")).toBeInTheDocument()
+    );
   });
 });
