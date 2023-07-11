911e14a3b3b0a719c43c496b926cd49d687b4556
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-07-11 19:53:16 +0900

    fix icon

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -10,6 +10,7 @@ import {
   FaBars,
   FaTimes,
 } from "react-icons/fa";
+import { RiFunctionFill } from "react-icons/ri";
 import { useAuthStore } from "../../state/store";
 import { logoutUser } from "../../services/api";
 
@@ -18,8 +19,10 @@ export const Header: React.FC = () => {
 
   const [isUserOpen, setIsUserOpen] = useState(false);
   const [adminIsOpen, setAdminIsOpen] = useState(false);
+  const [isServiceOpen, setIsServiceOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement | null>(null);
   const adminDropdownRef = useRef<HTMLDivElement | null>(null);
+  const serviceDropdownRef = useRef<HTMLDivElement | null>(null);
   const { userInfo, logout } = useAuthStore();
 
   useEffect(() => {
@@ -36,6 +39,12 @@ export const Header: React.FC = () => {
       ) {
         setAdminIsOpen(false);
       }
+      if (
+        serviceDropdownRef.current &&
+        !serviceDropdownRef.current.contains(e.target as Node)
+      ) {
+        setIsServiceOpen(false);
+      }
     };
 
     document.addEventListener("mousedown", closeDropdown);
@@ -50,126 +59,263 @@ export const Header: React.FC = () => {
             {userInfo && userInfo.isAdmin && <>Admin</>}TypeScriptShop
           </span>
         </Link>
-        <div className="sm:hidden">
-          <button onClick={() => setIsUserOpen(!isUserOpen)}>
-            {isUserOpen ? <FaTimes /> : <FaBars />}
-          </button>
-        </div>
-        <div className={`space-x-8 ${isUserOpen ? "block" : "hidden"} sm:flex`}>
-          {userInfo ? (
-            <div className="relative inline-block text-left" ref={dropdownRef}>
-              <div className="group">
-                <button
-                  type="button"
-                  onClick={() => setIsUserOpen(!isUserOpen)}
-                  className="flex items-center space-x-2 px-4 py-2 text-custom-blue-light hover:bg-custom-blue-lighter hover:text-custom-blue-extra-darkest"
-                >
-                  <FaUser className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
-                  <span
-                    data-testid="user-info-name"
-                    className="text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest"
-                  >
-                    {userInfo.name}
-                  </span>
-                  {isUserOpen ? (
-                    <FaChevronUp className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
-                  ) : (
-                    <FaChevronDown className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
-                  )}
-                </button>
-              </div>
 
-              {isUserOpen && (
-                <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-custom-blue-darker shadow-lg ring-1 ring-custom-blue-darker">
-                  <div
-                    className="py-1"
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="options-menu"
+        <div className="flex">
+          <div className="sm:hidden">
+            <button onClick={() => setIsServiceOpen(!isServiceOpen)}>
+              {isServiceOpen ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
+          <div
+            className={`space-x-8 ${
+              isServiceOpen ? "block" : "hidden"
+            } sm:flex`}
+          >
+            {userInfo ? (
+              <div
+                className="relative inline-block text-left"
+                ref={serviceDropdownRef}
+              >
+                <div className="group">
+                  <button
+                    type="button"
+                    onClick={() => setIsServiceOpen(!isServiceOpen)}
+                    className="flex items-center space-x-2 px-4 py-2 text-custom-blue-light hover:bg-custom-blue-lighter hover:text-custom-blue-extra-darkest"
                   >
-                    {!userInfo.isAdmin && (
+                    <RiFunctionFill className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
+                    <span
+                      data-testid="user-info-name"
+                      className="text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest"
+                    >
+                      Service
+                    </span>
+                    {isServiceOpen ? (
+                      <FaChevronUp className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
+                    ) : (
+                      <FaChevronDown className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
+                    )}
+                  </button>
+                </div>
+
+                {isServiceOpen && (
+                  <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-custom-blue-darker shadow-lg ring-1 ring-custom-blue-darker">
+                    <div
+                      className="py-1"
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="options-menu"
+                    >
+                      {!userInfo.isAdmin && (
+                        <Link
+                          to="/users"
+                          className="block px-4 py-2 text-sm text-custom-blue-lighter hover:bg-custom-blue-darkest"
+                          role="menuitem"
+                        >
+                          Users
+                        </Link>
+                      )}
+
                       <Link
-                        to="/users"
+                        to="/profile"
+                        className="block px-4 py-2 text-sm text-custom-blue-lighter hover:bg-custom-blue-darkest"
+                        role="menuitem"
+                      >
+                        Profile
+                      </Link>
+                      <button
+                        onClick={async () => {
+                          await logoutUser();
+                          logout();
+                          navigate("/login");
+                        }}
+                        className="block w-full px-4 py-2 text-left text-sm text-custom-blue-lighter hover:bg-custom-blue-darkest"
+                        role="menuitem"
+                      >
+                        Logout
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <Link
+                to="/login"
+                className="flex items-center space-x-2 text-custom-blue-lighter"
+              >
+                <FaUser className="h-5 w-5 text-custom-blue-lightest" />
+                <span className="text-custom-blue-lightest">Log In</span>
+              </Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div
+                className="relative inline-block text-left"
+                ref={adminDropdownRef}
+              >
+                <div className="group">
+                  <button
+                    type="button"
+                    onClick={() => setAdminIsOpen(!adminIsOpen)}
+                    className="flex items-center space-x-2 px-4 py-2 text-custom-blue-light hover:bg-custom-blue-lighter group-hover:text-custom-blue-extra-darkest"
+                  >
+                    <span className="text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest">
+                      Admin Function
+                    </span>
+                    {adminIsOpen ? (
+                      <FaChevronUp className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
+                    ) : (
+                      <FaChevronDown className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
+                    )}
+                  </button>
+                </div>
+
+                {adminIsOpen && (
+                  <div className="absolute  right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-custom-blue-darker shadow-lg ring-1 ring-custom-blue-darker">
+                    <div
+                      className="py-1"
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="options-menu"
+                    >
+                      <Link
+                        to="/admin/users"
                         className="block px-4 py-2 text-sm text-custom-blue-lighter hover:bg-custom-blue-darkest"
                         role="menuitem"
                       >
                         Users
                       </Link>
-                    )}
-
-                    <Link
-                      to="/profile"
-                      className="block px-4 py-2 text-sm text-custom-blue-lighter hover:bg-custom-blue-darkest"
-                      role="menuitem"
-                    >
-                      Profile
-                    </Link>
-                    <button
-                      onClick={async () => {
-                        await logoutUser();
-                        logout();
-                        navigate("/login");
-                      }}
-                      className="block w-full px-4 py-2 text-left text-sm text-custom-blue-lighter hover:bg-custom-blue-darkest"
-                      role="menuitem"
-                    >
-                      Logout
-                    </button>
+                    </div>
                   </div>
-                </div>
-              )}
-            </div>
-          ) : (
-            <Link
-              to="/login"
-              className="flex items-center space-x-2 text-custom-blue-lighter"
-            >
-              <FaUser className="h-5 w-5 text-custom-blue-lightest" />
-              <span className="text-custom-blue-lightest">Log In</span>
-            </Link>
-          )}
-          {userInfo && userInfo.isAdmin && (
-            <div
-              className="relative inline-block text-left"
-              ref={adminDropdownRef}
-            >
-              <div className="group">
-                <button
-                  type="button"
-                  onClick={() => setAdminIsOpen(!adminIsOpen)}
-                  className="flex items-center space-x-2 px-4 py-2 text-custom-blue-light hover:bg-custom-blue-lighter group-hover:text-custom-blue-extra-darkest"
-                >
-                  <span className="text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest">
-                    Admin Function
-                  </span>
-                  {adminIsOpen ? (
-                    <FaChevronUp className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
-                  ) : (
-                    <FaChevronDown className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
-                  )}
-                </button>
+                )}
               </div>
+            )}
+          </div>
 
-              {adminIsOpen && (
-                <div className="absolute  right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-custom-blue-darker shadow-lg ring-1 ring-custom-blue-darker">
-                  <div
-                    className="py-1"
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="options-menu"
+          <div className="sm:hidden">
+            <button onClick={() => setIsUserOpen(!isUserOpen)}>
+              {isUserOpen ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
+          <div
+            className={`space-x-8 ${isUserOpen ? "block" : "hidden"} sm:flex`}
+          >
+            {userInfo ? (
+              <div
+                className="relative inline-block text-left"
+                ref={dropdownRef}
+              >
+                <div className="group">
+                  <button
+                    type="button"
+                    onClick={() => setIsUserOpen(!isUserOpen)}
+                    className="flex items-center space-x-2 px-4 py-2 text-custom-blue-light hover:bg-custom-blue-lighter hover:text-custom-blue-extra-darkest"
                   >
-                    <Link
-                      to="/admin/users"
-                      className="block px-4 py-2 text-sm text-custom-blue-lighter hover:bg-custom-blue-darkest"
-                      role="menuitem"
+                    <FaUser className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
+                    <span
+                      data-testid="user-info-name"
+                      className="text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest"
                     >
-                      Users
-                    </Link>
-                  </div>
+                      {userInfo.name}
+                    </span>
+                    {isUserOpen ? (
+                      <FaChevronUp className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
+                    ) : (
+                      <FaChevronDown className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
+                    )}
+                  </button>
                 </div>
-              )}
-            </div>
-          )}
+
+                {isUserOpen && (
+                  <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-custom-blue-darker shadow-lg ring-1 ring-custom-blue-darker">
+                    <div
+                      className="py-1"
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="options-menu"
+                    >
+                      {!userInfo.isAdmin && (
+                        <Link
+                          to="/users"
+                          className="block px-4 py-2 text-sm text-custom-blue-lighter hover:bg-custom-blue-darkest"
+                          role="menuitem"
+                        >
+                          Users
+                        </Link>
+                      )}
+
+                      <Link
+                        to="/profile"
+                        className="block px-4 py-2 text-sm text-custom-blue-lighter hover:bg-custom-blue-darkest"
+                        role="menuitem"
+                      >
+                        Profile
+                      </Link>
+                      <button
+                        onClick={async () => {
+                          await logoutUser();
+                          logout();
+                          navigate("/login");
+                        }}
+                        className="block w-full px-4 py-2 text-left text-sm text-custom-blue-lighter hover:bg-custom-blue-darkest"
+                        role="menuitem"
+                      >
+                        Logout
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <Link
+                to="/login"
+                className="flex items-center space-x-2 text-custom-blue-lighter"
+              >
+                <FaUser className="h-5 w-5 text-custom-blue-lightest" />
+                <span className="text-custom-blue-lightest">Log In</span>
+              </Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div
+                className="relative inline-block text-left"
+                ref={adminDropdownRef}
+              >
+                <div className="group">
+                  <button
+                    type="button"
+                    onClick={() => setAdminIsOpen(!adminIsOpen)}
+                    className="flex items-center space-x-2 px-4 py-2 text-custom-blue-light hover:bg-custom-blue-lighter group-hover:text-custom-blue-extra-darkest"
+                  >
+                    <span className="text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest">
+                      Admin Function
+                    </span>
+                    {adminIsOpen ? (
+                      <FaChevronUp className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
+                    ) : (
+                      <FaChevronDown className="h-5 w-5 text-custom-blue-lightest group-hover:text-custom-blue-extra-darkest" />
+                    )}
+                  </button>
+                </div>
+
+                {adminIsOpen && (
+                  <div className="absolute  right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-custom-blue-darker shadow-lg ring-1 ring-custom-blue-darker">
+                    <div
+                      className="py-1"
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="options-menu"
+                    >
+                      <Link
+                        to="/admin/users"
+                        className="block px-4 py-2 text-sm text-custom-blue-lighter hover:bg-custom-blue-darkest"
+                        role="menuitem"
+                      >
+                        Users
+                      </Link>
+                    </div>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </header>
