3fe623eadac916e05f3b6337949b810bfa3ab6c6
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-07-16 12:08:41 +0900

    fix UserScreen.test

--- frontend/src/__test__/screen/user/UserScreen.test.tsx
+++ frontend/src/__test__/screen/user/UserScreen.test.tsx
@@ -651,6 +651,241 @@ describe("UserScreen", () => {
     // });
   });
 
+  it("Fail server test Unfollow", async () => {
+    server.use(
+      rest.get("http://localhost:8080/api/users/1/posts", (_req, res, ctx) => {
+        return res(
+          ctx.json({
+            user: {
+              id: 3,
+              name: "User",
+              email: "user@email.com",
+              password:
+                "$2a$10$RbWiHaEGQeCMAHwOtqSV6.otv7FUY.9JCwGb1/DkE0Ic.H4vXkHu2",
+              avatarPath: null,
+              isAdmin: false,
+              createdAt: "2023-07-10T14:24:43.958Z",
+              updatedAt: "2023-07-10T14:24:43.958Z",
+              posts: [
+                {
+                  id: 1,
+                  userId: 2,
+                  description: "post_description2",
+                  imagePath: "image_url2",
+                  createdAt: "2023-07-10T14:24:44.121Z",
+                  updatedAt: "2023-07-10T14:24:44.121Z",
+                  user: {
+                    id: 2,
+                    name: "User",
+                    avatarPath: null,
+                  },
+                },
+                {
+                  id: 2,
+                  userId: 2,
+                  description: "post_description1",
+                  imagePath: "image_url1",
+                  createdAt: "2023-07-10T14:24:44.121Z",
+                  updatedAt: "2023-07-10T14:24:44.121Z",
+                  user: {
+                    id: 2,
+                    name: "User",
+                    avatarPath: null,
+                  },
+                },
+              ],
+              followedBy: [
+                {
+                  id: 2,
+                  followee: {
+                    id: 5,
+                    name: "User4",
+                    email: "user4@email.com",
+                  },
+                },
+                {
+                  id: 3,
+                  followee: {
+                    id: 4,
+                    name: "User3",
+                    email: "user3@email.com",
+                  },
+                },
+                {
+                  id: 7,
+                  followee: {
+                    id: 3,
+                    name: "User2",
+                    email: "user2@email.com",
+                  },
+                },
+              ],
+              _count: {
+                followedBy: 3,
+                following: 4,
+              },
+              isFollowed: true,
+              followerCount: 3,
+              followeeCount: 4,
+            },
+            posts: [
+              {
+                id: 1,
+                userId: 2,
+                description: "post_description2",
+                imagePath: "image_url2",
+                createdAt: "2023-07-10T14:24:44.121Z",
+                updatedAt: "2023-07-10T14:24:44.121Z",
+                user: {
+                  id: 2,
+                  name: "User",
+                  avatarPath: null,
+                },
+              },
+              {
+                id: 2,
+                userId: 2,
+                description: "post_description1",
+                imagePath: "image_url1",
+                createdAt: "2023-07-10T14:24:44.121Z",
+                updatedAt: "2023-07-10T14:24:44.121Z",
+                user: {
+                  id: 2,
+                  name: "User",
+                  avatarPath: null,
+                },
+              },
+            ],
+          })
+        );
+      }),
+      rest.delete(
+        "http://localhost:8080/api/users/follow/3",
+        (_req, res, ctx) => {
+          return res(ctx.status(500), ctx.json({ message: "Server Error" }));
+        }
+      ),
+      rest.get("http://localhost:8080/api/users/3/posts", (_req, res, ctx) => {
+        return res(
+          ctx.json({
+            user: {
+              id: 3,
+              name: "User",
+              email: "user@email.com",
+              password:
+                "$2a$10$RbWiHaEGQeCMAHwOtqSV6.otv7FUY.9JCwGb1/DkE0Ic.H4vXkHu2",
+              avatarPath: null,
+              isAdmin: false,
+              createdAt: "2023-07-10T14:24:43.958Z",
+              updatedAt: "2023-07-10T14:24:43.958Z",
+              posts: [
+                {
+                  id: 1,
+                  userId: 2,
+                  description: "post_description2",
+                  imagePath: "image_url2",
+                  createdAt: "2023-07-10T14:24:44.121Z",
+                  updatedAt: "2023-07-10T14:24:44.121Z",
+                  user: {
+                    id: 2,
+                    name: "User",
+                    avatarPath: null,
+                  },
+                },
+                {
+                  id: 2,
+                  userId: 2,
+                  description: "post_description1",
+                  imagePath: "image_url1",
+                  createdAt: "2023-07-10T14:24:44.121Z",
+                  updatedAt: "2023-07-10T14:24:44.121Z",
+                  user: {
+                    id: 2,
+                    name: "User",
+                    avatarPath: null,
+                  },
+                },
+              ],
+              followedBy: [
+                {
+                  id: 2,
+                  followee: {
+                    id: 5,
+                    name: "User4",
+                    email: "user4@email.com",
+                  },
+                },
+                {
+                  id: 3,
+                  followee: {
+                    id: 4,
+                    name: "User3",
+                    email: "user3@email.com",
+                  },
+                },
+                {
+                  id: 7,
+                  followee: {
+                    id: 3,
+                    name: "User2",
+                    email: "user2@email.com",
+                  },
+                },
+              ],
+              _count: {
+                followedBy: 3,
+                following: 4,
+              },
+              isFollowed: true,
+              followerCount: 3,
+              followeeCount: 4,
+            },
+            posts: [
+              {
+                id: 1,
+                userId: 2,
+                description: "post_description2",
+                imagePath: "image_url2",
+                createdAt: "2023-07-10T14:24:44.121Z",
+                updatedAt: "2023-07-10T14:24:44.121Z",
+                user: {
+                  id: 2,
+                  name: "User",
+                  avatarPath: null,
+                },
+              },
+              {
+                id: 2,
+                userId: 2,
+                description: "post_description1",
+                imagePath: "image_url1",
+                createdAt: "2023-07-10T14:24:44.121Z",
+                updatedAt: "2023-07-10T14:24:44.121Z",
+                user: {
+                  id: 2,
+                  name: "User",
+                  avatarPath: null,
+                },
+              },
+            ],
+          })
+        );
+      })
+    );
+
+    await setup();
+
+    const button = await screen.findByRole("button", {
+      name: /(Follow|Unfollow)/i,
+    });
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      const errorMessage = screen.getByRole("alert");
+      expect(errorMessage).toHaveTextContent("Server Error");
+    });
+  });
+
   it("server error renders the UserScreen and shows user details", async () => {
     server.use(
       rest.get("http://localhost:8080/api/users/1/posts", (_req, res, ctx) => {
