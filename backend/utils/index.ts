246e0e79f10173f716119cd5c6f44ed0f5314ff6
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-07-22 23:00:23 +0900

    add com

--- backend/utils/index.ts
+++ backend/utils/index.ts
@@ -24,3 +24,9 @@ export const generateToken = (res: Response, userId: UserType["id"]): void => {
 export const hashPassword = async (password: string): Promise<string> => {
   return await bcrypt.hash(password, 10);
 };
+
+// Todo Utilsに下記をまとめる
+// エラーハンドリングユーティリティ: エラーメッセージの形式を一貫させ、エラーログを生成したり、特定のエラーに基づいて特定のHTTPステータスコードをレスポンスするなどの機能を提供する。
+// 認証・認可ユーティリティ: ユーザーの認証や認可を処理するユーティリティ。JWTトークンの生成や検証、セッション管理、権限に基づくルート保護などが含まれます。
+// 日付・時間ユーティリティ: 一貫した日付と時間の形式を保証し、時刻の変換や操作を行う。
+// パスワードハッシュ・検証ユーティリティ: パスワードのハッシュ化と検証を行う。
